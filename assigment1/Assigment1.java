a44ceaee546004441201b56bd800e54acc439f72
vaibhav567 <vaibhav17890@gmail.com>
2015-11-25 17:58:39 +0530

    updated content

---
+++ assigment1/Assigment1.java
@@ -0,0 +1,15 @@
+package assigment1;
+
+public class Assigment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10,aq;
+float b = 10.5f;
+float qqf;
+qqf = a + b;
+//to convert interger to float see below
+//qqf = a+(int)b;
+System.out.println("the sum of 2 no = " +qqf);
+}
+}
